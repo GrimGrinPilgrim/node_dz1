95ed4f7434541c64c3c918ad84e940615c1fd1cf
kseniy <kseniy94-94@mail.ru>
2015-03-12 09:02:58 +0000

    dz

--- lesson_2/app.js
+++ lesson_2/app.js
@@ -1,9 +1,12 @@
 var express = require('express'),
   app = express(),
-  haml = require('haml'),
+  haml = require('hamljs'),
   fs = require('fs');
 
 app.use(express.static('photos'));
+app.use(express.static('404img'));
+
+app.use(express.static(__dirname + '/css'));
 
 app.get('/photos/:id',function(req,res){
   var hero_name = req.params.id
@@ -14,8 +17,13 @@ app.get('/photos/:id',function(req,res){
   res.end( haml.render(view, { locals:{ hero: hero_name, hero_img: hero_img} }) );
 })
 
+app.engine('.haml', require('hamljs').renderFile);
+
 app.get('*',function(req,res){
-  res.end('404')
+  var view = fs.readFileSync('404.haml', 'utf8');
+  res.setHeader("Content-Type", "text/html; charset=utf8");
+  res.end( haml.render(view) );
 })
+
 app.listen('4444')
 console.log('all good')
